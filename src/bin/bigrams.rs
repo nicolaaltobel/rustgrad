669e16001e4d41141658e23d1d3f82a2dfb811cd
Nicola <nicola.altobel@studenti.unitn.it>
2026-04-26 20:01:15 +0200

    Fixed bigram example

--- src/bin/bigrams.rs
+++ src/bin/bigrams.rs
@@ -1,7 +1,9 @@
-use ndarray::{Array, IxDyn};
+use ndarray::{stack, Array, Axis, Ix2, IxDyn};
+use rand::{Rng, SeedableRng};
+use rand::prelude::StdRng;
 use rustgrad::Activation::Tanh;
 use rustgrad::Init::He;
-use rustgrad::Mlp;
+use rustgrad::{Mlp, Tensor};
 
 fn main(){
     let mut names : Vec<String> = Vec::new();
@@ -23,11 +25,11 @@ fn main(){
             if let Ok(i) = ctoi(w[0]) && let Ok(j) = ctoi(w[1]){
                 probs[[i,j]] += 1.;
                 xs.push(one_hot(i));
-                ys.push(one_hot(j));
+                ys.push(j);
             }
         }
     }
-    println!("{probs}");
+    //println!("{probs}");
 
     /*for k in 1..27 {
         let mut start = itoc(k);
@@ -51,6 +53,64 @@ fn main(){
 
 
     let nn = Mlp::new(&[27,27], He, Tanh);
+
+    let x_batch = Tensor::leaf(
+        stack(
+            Axis(0),
+            &xs.iter().map(|x| x.view()).collect::<Vec<_>>(),
+        ).unwrap().into_dyn()
+    );
+
+    for epoch in 1..=100{
+        let logits = nn.forward(&x_batch);
+        let loss = logits.log_softmax(1).nll_loss(&ys);
+
+        nn.zero_grad();
+        loss.backward();
+        nn.update(10.);
+
+        println!("epoch {epoch}, loss: {loss}");
+    }
+    let mut rng = StdRng::seed_from_u64(42);
+    for _ in 0..100 {
+        let mut c = '.';
+        let mut name = String::new();
+        loop {
+            let x = Tensor::leaf(
+                one_hot(ctoi(c).unwrap())
+                    .into_shape_with_order((1, 27))
+                    .unwrap()
+                    .into_dyn(),
+            );
+            let log_probs = nn.forward(&x).log_softmax(1);
+            let data = log_probs
+                .data()
+                .into_dimensionality::<Ix2>()
+                .unwrap();
+            let probs = data.row(0).mapv(f64::exp);
+
+            // campiona dalla distribuzione
+            let r: f64 = rng.random();
+            let mut cumsum = 0.0;
+            let next_idx = probs
+                .iter()
+                .enumerate()
+                .find(|(_, p)| {
+                    cumsum += *p;
+                    cumsum >= r
+                })
+                .map_or(probs.len()-1, |(i, _)| i);
+
+            c = itoc(next_idx);
+            if c == '.' {
+                break;
+            }
+            name.push(c);
+            //print!("{c}");
+            //stdout().flush().unwrap();
+        }
+        println!("{name}");
+    }
 }
 
 fn ctoi (mut c : char) -> Result<usize, String>{
